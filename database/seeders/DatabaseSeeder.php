0450844d59c0d2434d0aef26b84de2dadf4b54d0
مها عبدالرزاق التركي <altrkymha12@gmail.com>
2026-01-15 15:56:34 +0200

    feat: add initial DatabaseSeeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,11 +20,11 @@ class DatabaseSeeder extends Seeder
         User::updateOrCreate(
             ['email' => 'doaa@gmail.com'], // للبحث إذا كان المستخدم موجود مسبقاً
             [
-                'full_name'      => 'Doaa',
-                'phone_number'   => '0924576189', 
-                'password'       => Hash::make('0924576189'), 
-                'role'           => 'admin', 
-                'account_status' => 'active', // لضمان عدم ظهور خطأ الحقل الفارغ
+                'full_name' => 'Doaa',
+                'phone_number' => '0924576189',
+                'password' => Hash::make('0924576189'),
+                'role' => 'ADMIN',
+                'account_status' => 'ACTIVE', // لضمان عدم ظهور خطأ الحقل الفارغ
             ]
         );
     }
